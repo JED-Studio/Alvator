8f77371c0e94f1b0602eac66e981007fead0cb3a
Ramazan <romagt2015@mail.ru>
2024-05-06 21:03:54 +0300

    header

--- Alvator/src/App.vue
+++ Alvator/src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+
 
 </script>
 
 <template>
-  <header>
- 
-  </header>
+<div class="lok bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+<header class="flex">
+  <div class="">
+    <img src="" alt="">
+  </div>
+  <div class="">
+
+  </div>
+</header>
+</div>
 </template>
 
 <style scoped>
