4f83ba5b6aa502b523422170614b5b1ef9848370
Ramazan <romagt2015@mail.ru>
2024-05-08 22:34:26 +0300

    header

--- Alvator/src/App.vue
+++ Alvator/src/App.vue
@@ -1,18 +1,31 @@
 <script setup>
-
+import Time from './components/Time.vue';
 
 </script>
 
 <template>
-<div class="lok bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
-<header class="flex">
+<div class="">
+<header class="mt-4">
+  <div class="max-w-7xl mx-auto pl-4 pr-4">
+    <div class="flex justify-between justify-items-center items-center">
   <div class="">
-    <img src="" alt="">
+    <img src="/public/LOLO1.png" alt="" width="70px">
   </div>
-  <div class="">
+  <nav class="text-white">
+    <ul class="flex">
+    <li class="">Главная</li>
+    <li class="">Цены</li>
+    <li class="">Игры</li>
+    <li class="">Системы</li>
+    </ul>
+  </nav>
+  <div>
 
   </div>
+  </div>
+</div>
 </header>
+<Time/>
 </div>
 </template>
 
