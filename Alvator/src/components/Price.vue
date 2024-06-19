c3fc69f92a9fb959f13a7c3b920a62c001ff58ea
Ramazan <romagt2015@mail.ru>
2024-06-20 00:59:02 +0300

    price

--- Alvator/src/components/Price.vue
+++ Alvator/src/components/Price.vue
@@ -2,12 +2,27 @@
     <section class="mt-4">
     <div class="max-w-7xl mx-auto pl-4 pr-4">
         <div class="flex">
-            <div>
-                <div class="rounded-3xl border-white border p-8  text-white">1 час</div>
-                <div class="rounded-3xl border-white border p-8  text-white">3 час</div>
-                <div class="rounded-3xl border-white border p-8  text-white">5 час</div>
+            <div class="max-w-xs w-full">
+                <div class="rounded-3xl border-white border p-8  text-white flex justify-between">
+                    <p>1 час</p>
+                    <p>100р</p>
+                </div>
+                <div class="rounded-3xl border-white border p-8  text-white flex justify-between">
+                    <p>3 час</p>
+                    <p>200р</p>
+                </div>
+                <div class="rounded-3xl border-white border p-8  text-white flex justify-between">
+                    <p>5 час</p>
+                    <p>300р</p>
+                </div>
+                
             </div>
-            <div class="rounded-3xl border-white border p-8"></div>
+            <div class="text-white">
+                <p>Утро</p>
+                <div class="rounded-3xl border-white border p-8">6:00-12:00</div>
+                <p>Ночь</p>
+                <div class="rounded-3xl border-white border p-8">21:00-6:00</div>
+            </div>
             
         </div>
     </div>
