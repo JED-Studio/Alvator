0fa87a209502f2bc89f7262c91aa5f8cf451d669
Ramazan <romagt2015@mail.ru>
2024-06-25 21:00:05 +0300

    Price card

--- Alvator/src/components/Price.vue
+++ Alvator/src/components/Price.vue
@@ -1,30 +1,82 @@
 <template>
     <section class="mt-4">
     <div class="max-w-7xl mx-auto pl-4 pr-4">
-        <div class="flex">
-            <div class="max-w-xs w-full">
-                <div class="rounded-3xl border-white border p-8  text-white flex justify-between">
+        <div class="flex gap-16 justify-center">
+            <div class="max-w-xs w-full border rounded-3xl p-5 flex flex-col gap-3 ">
+                <div class="text-white text-center text-2xl">ОБЫЧНЫЙ ЗАЛ</div>
+                <div class="rounded-xl  border-white border p-4 text-white flex justify-between">
                     <p>1 час</p>
                     <p>100р</p>
                 </div>
-                <div class="rounded-3xl border-white border p-8  text-white flex justify-between">
+                <div class="rounded-xl border-white border p-4  text-white flex justify-between">
                     <p>3 час</p>
                     <p>200р</p>
                 </div>
-                <div class="rounded-3xl border-white border p-8  text-white flex justify-between">
+                <div class="rounded-xl border-white border p-4  text-white flex justify-between">
                     <p>5 час</p>
                     <p>300р</p>
                 </div>
+                <div class="text-white">
+                <p>Утро</p>
+                <div class="rounded-xl border-white border p-4">6:00-12:00</div>
+                <p>Ночь</p>
+                <div class="rounded-xl border-white border p-4">21:00-6:00</div>
+            </div>
                 
             </div>
-            <div class="text-white">
+            <div class="max-w-xs w-full border rounded-3xl p-5 flex flex-col gap-3">
+                <div class="text-white text-center text-2xl">VIP</div>
+                <div class="rounded-xl border-white border p-4 text-white flex justify-between">
+                    <p>1 час</p>
+                    <p>200р</p>
+                </div>
+                <div class="rounded-xl border-white border p-4  text-white flex justify-between">
+                    <p>3 час</p>
+                    <p>400р</p>
+                </div>
+                <div class="rounded-xl border-white border p-4  text-white flex justify-between">
+                    <p>5 час</p>
+                    <p>900р</p>
+                </div>
+                <div class="text-white">
                 <p>Утро</p>
-                <div class="rounded-3xl border-white border p-8">6:00-12:00</div>
+                <div class="rounded-xl border-white border p-4">6:00-12:00</div>
                 <p>Ночь</p>
-                <div class="rounded-3xl border-white border p-8">21:00-6:00</div>
+                <div class="rounded-xl border-white border p-4">21:00-6:00</div>
             </div>
+                
+            </div>
+        
+            <div class="max-w-xs w-full border rounded-3xl p-5 flex flex-col gap-3">
+                <div class="text-white text-center text-2xl">PlayStation 5</div>
+                <div class="rounded-xl border-white border p-4 text-white flex justify-between">
+                    <p>1 час</p>
+                    <p>150р</p>
+                </div>
+                <div class="rounded-xl border-white border p-4  text-white flex justify-between">
+                    <p>3 час</p>
+                    <p>300р</p>
+                </div>
+                <div class="rounded-xl border-white border p-4  text-white flex justify-between">
+                    <p>5 час</p>
+                    <p>600р</p>
+                </div>
+                <div class="text-white">
+                <p>Утро</p>
+                <div class="rounded-xl border-white border p-4">6:00-12:00</div>
+                <p>Ночь</p>
+                <div class="rounded-xl border-white border p-4">21:00-6:00</div>
+            </div>
+                
+            </div>
+        
+           
             
         </div>
     </div>
     </section>
 </template>
+
+<style>
+
+</style>
